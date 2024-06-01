017f6c9643a5357aad59fa5f92387cd2c80d2e08
Charles Beall <wacharlb@gmail.com>
2024-06-01 01:11:27 -0700

    Modified index.js to add deployment environment variable.

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ const server = require("./server");
 const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1/330-final-project";
 
-mongoose
-  .connect("mongodb://127.0.0.1/330-final-project", {})
-  .then(() => {
-    server.listen(port, () => {
-      console.log(`Server is listening on http://localhost:${port}`);
-    });
-  })
-  .catch((e) => {
-    console.error(`Failed to start server:`, e);
+mongoose.connect(mongoURL, {}).then(() => {
+  server.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
   });
+}).catch((e) => {
+  console.error(`Failed to start server:`, e);
+});
